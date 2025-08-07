473763b01a8f1b5cc65faed00a1df159c4e244af
rzaynuri08 <rzaynuri08@gmail.com>
2025-08-07 12:42:50 +0700

    add fillable var in model genus

--- app/Models/Genus.php
+++ app/Models/Genus.php
@@ -9,6 +9,12 @@ class Genus extends Model
     protected $table = 'genus';
     protected $primaryKey = 'id_genus';
 
+    protected $fillable = [
+        'name',        // kolom nama genus
+        'id_prevention',
+        'id_disease'
+    ];
+
     public function prevention()
     {
         return $this->belongsTo(Prevention::class, 'id_prevention');
